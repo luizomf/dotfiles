b7e492b9165eee7e347c9955e4c5731e882c1c5e
Luiz Otávio <luizomf@gmail.com>
2025-07-24 17:11:44 -0300

    recreate install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-# Funcao para printar logs
+# Função para printar logs de forma legível
 log() {
-  echo -e "\n\033[1;34m$1\033[0m"
+  echo -e "
+[1;34m$1[0m"
 }
+
+# Garante que o script pare em caso de erro
+set -e
 
 # --- Homebrew e Brewfile ---
 log "🍺 Verificando e instalando dependências com Homebrew..."
 if ! command -v brew &> /dev/null; then
   log "Homebrew não encontrado. Instalando..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> "$HOME/.zprofile"
   eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   log "Homebrew já está instalado. Atualizando..."
   brew update
 fi
 
-# Instala tudo do Brewfile
-brew bundle --file=~/dotfiles/homebrew/Brewfile
+log "📦 Instalando pacotes e aplicações do Brewfile..."
+brew bundle --file="$HOME/dotfiles/homebrew/Brewfile"
 
 # --- Zsh e Oh My Zsh ---
-log "셸 Verificando e instalando Oh My Zsh e plugins..."
+log "셸 Configurando Zsh e Oh My Zsh..."
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   log "Instalando Oh My Zsh..."
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -29,41 +33,59 @@ else
   log "Oh My Zsh já está instalado."
 fi
 
-# Plugins do Zsh
+# Instala plugins do Zsh
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+log "🔌 Instalando plugins do Zsh..."
 if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-autosuggestions" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
 fi
 if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting" ]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
 fi
 
-# --- Gerenciadores de Plugins (Vim e Tmux) ---
-log "🔌 Instalando gerenciadores de plugins..."
-
-# vim-plug para Vim
-if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# --- Configuração do Neovim com Lazy.nvim ---
+log "🐘 Configurando Neovim e Lazy.nvim..."
+LAZY_PATH="$HOME/.local/share/nvim/lazy/lazy.nvim"
+if [ ! -d "$LAZY_PATH" ]; then
+  log "Instalando o gerenciador de plugins Lazy.nvim..."
+  git clone https://github.com/folke/lazy.nvim.git --filter=blob:none "$LAZY_PATH"
 fi
 
-# tpm para Tmux
+# --- Gerenciador de Plugins do Tmux (TPM) ---
+log "🔄 Instalando TPM para Tmux..."
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-# --- Symlinks ---
-log "🔗 Criando symlinks..."
-ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/zsh/.zprofile ~/.zprofile
-ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
+# --- Criação de Symlinks ---
+log "🔗 Criando symlinks para os arquivos de configuração..."
+
+# Cria o diretório ~/.config se não existir
+mkdir -p "$HOME/.config"
+
+# Zsh
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.zshrc"
+ln -sf "$HOME/dotfiles/zsh/.zprofile" "$HOME/.zprofile"
+
+# Git
+ln -sf "$HOME/dotfiles/git/.gitconfig" "$HOME/.gitconfig"
+
+# Tmux
+ln -sf "$HOME/dotfiles/tmux/.tmux.conf" "$HOME/.tmux.conf"
+
+# Vim (para compatibilidade)
+ln -sf "$HOME/dotfiles/vim/.vimrc" "$HOME/.vimrc"
+
+# Neovim
+ln -sf "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
+
+# Ghostty
+ln -sf "$HOME/dotfiles/ghostty" "$HOME/.config/ghostty"
 
 # --- Finalização ---
 log "✅ Script de instalação concluído!"
-echo -e "\n\033[1;33mATENÇÃO: Passos manuais necessários:\033[0m"
-echo "1. Inicie o Vim e execute \`:PlugInstall\` para instalar os plugins."
-echo "2. Inicie o Tmux e pressione \`prefix + I\` (Ctrl+b + I) para instalar os plugins."
+echo -e "
+[1;33mATENÇÃO: Passos manuais necessários:[0m"
+echo "1. Abra o Neovim ('nvim') para que o Lazy.nvim possa instalar todos os plugins."
+echo "2. Inicie o Tmux e pressione 'prefix + I' (Ctrl+b + I) para instalar os plugins do TPM."
 echo "3. Reinicie seu terminal para que todas as alterações tenham efeito."
-
