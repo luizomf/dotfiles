6bf38a59dca710df2e91d342f933492cc8f569b5
Luiz Otávio <luizomf@gmail.com>
2025-07-28 17:38:44 -0300

    start creating new_install script

--- new_install.sh
+++ new_install.sh
@@ -70,8 +70,8 @@ elif [[ "$OP_SYSTEM" == "ubuntu" ]]; then
   loginfo "Installing apps..."
   sudo apt-get install build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev wget curl \
-    llvm gettext libncurses5-dev tk-dev tcl-dev blt-dev libgdbm-dev \
-    git python2-dev python3-dev aria2 lzma liblzma-dev
+    llvm gettext tk-dev tcl-dev blt-dev libgdbm-dev \
+    git python3-dev aria2 lzma liblzma-dev
 
   loginfo "Installing apps..."
   sudo apt install \
