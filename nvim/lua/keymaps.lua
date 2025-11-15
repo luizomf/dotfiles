ca531ac5effbd995d1de2d45cd4baf81bb1a7cae
Luiz Otávio <luizomf@gmail.com>
2025-11-15 07:50:54 -0300

    refactor: keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -45,7 +45,11 @@ end, { desc = "Format file or range (conform)" })
 local builtin = require("telescope.builtin")
 map("n", "<leader>fre", builtin.registers, { desc = "Find in registers" })
 map("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-map("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+map("n", "<leader>fg", function()
+  builtin.live_grep({
+    grep_open_files = true,
+  })
+end, { desc = "Live grep (arquivos abertos)" })
 map("n", "<leader>fb", builtin.buffers, { desc = "List open buffers" })
 map("n", "<leader>fh", builtin.help_tags, { desc = "Search help tags" })
 map("n", "<leader>fo", builtin.oldfiles, { desc = "Recently opened files" })
