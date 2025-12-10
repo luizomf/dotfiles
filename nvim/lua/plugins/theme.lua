e34f24eb0ab49be54c886cd68f9f41e7b72ee240
Luiz Otávio <luizomf@gmail.com>
2025-12-10 10:57:00 -0300

    refactor: testing new theme background

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -4,6 +4,94 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      require("tokyonight").setup({
+        style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+        light_style = "day", -- The theme is used when the background is set to light
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = false, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
+        },
+        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+        --- You can override specific color groups to use other groups or a hex color
+        --- function will be called with a ColorScheme table
+        ---@param colors ColorScheme
+        on_colors = function(colors)
+          colors.black = "#0c0e14"
+
+          colors.bg_sidebar = "#0c0e14"
+          colors.bg_float = "#0c0e14"
+          colors.bg_popup = "#0c0e14"
+          colors.bg_statusline = "#0c0e14"
+          colors.comment = "#43455e"
+          colors.bg_visual = "#232534"
+
+          -- colors.red = "#f7768e"
+          -- colors.red1 = "#ff8787"
+          colors.green = "#5fffaf"
+          -- colors.yellow = "#ffffaf"
+          -- colors.teal = "#5fffd7"
+          -- colors.orange = "#ffaf00"
+          -- colors.blue0 = "#87ffff"
+          -- colors.blue1 = "#afd7ff"
+          -- colors.blue2 = "#5fd7ff"
+          -- colors.blue5 = "#00afff"
+          -- colors.blue6 = "#0087ff"
+          -- colors.blue7 = "#005fff"
+          -- colors.magenta = "#d7afff"
+          -- colors.magenta2 = "#af87ff"
+          -- colors.cyan = "#5fffff"
+          --
+          -- colors.bg = "#13141f"
+          -- colors.bg_dark = "#11131b"
+          -- colors.bg_dark1 = "#0c0e14"
+          --
+          -- colors.fg = "#ebeef5"
+          -- colors.fg_dark = "#bbc7e2"
+          -- colors.fg_gutter = "#343b52"
+          --
+          -- colors.dark3 = "#343b52"
+          -- colors.dark5 = "#00FF00"
+
+          colors.comment = "#52557c"
+        end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with a Highlights and ColorScheme table
+        ---@param highlights tokyonight.Highlights
+        ---@param colors ColorScheme
+        on_highlights = function(hl, c)
+          hl.CursorLine = { bg = "#232534" }
+          hl.Visual = { bg = "#232534" }
+        end,
+
+        cache = true, -- When set to true, the theme will be cached for better performance
+
+        ---@type table<string, boolean|{enabled:boolean}>
+        plugins = {
+          -- enable all plugins when not using lazy.nvim
+          -- set to false to manually enable/disable plugins
+          all = package.loaded.lazy == nil,
+          -- uses your plugin manager to automatically enable needed plugins
+          -- currently only lazy.nvim is supported
+          auto = true,
+          -- add any plugins here that you want to enable
+          -- for all possible plugins, see:
+          --   * https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
+          -- telescope = true,
+        },
+      })
       vim.cmd("colorscheme tokyonight-night")
     end,
   },
--- vim/.vimrc
+++ vim/.vimrc
@@ -1,4 +1,3 @@
-" --- Gerenciador de Plugins (vim-plug) ---
 call plug#begin('~/.vim/plugged')
 
 " Fuzzy finder para arquivos, buffers, etc.
@@ -9,62 +8,52 @@ call plug#end()
 
 " --- Atalhos para FZF ---
 " Ctrl+P para buscar arquivos (similar ao VSCode/Sublime)
-nnoremap <C-p> :Files<CR>
+"nnoremap <C-p> :Files<CR>
 
-" --- Básico ---
-set number              " mostra número das linhas
-syntax on               " highlight de sintaxe
-set tabstop=2           " tabulação com 2 espaços
+set number
+set relativenumber
+set nocursorline
+set colorcolumn=80 
+set backupcopy=yes 
+set expandtab
+syntax on
+set tabstop=2
 set shiftwidth=2
 set expandtab
 set autoindent
 set smartindent
-set mouse=a             " permite uso do mouse (opcional)
-set clipboard=unnamed   " usa clipboard do sistema (pode mudar pra unnamedplus se der problema)
-set nowrap              " não quebra linhas automaticamente
+set mouse=a
 
-" --- Histórico e posição ---
-set viminfo=            " desativa histórico persistente (registers, marks etc.)
-" Caso queira manter histórico mínimo, substitua por:
-" set viminfo='20,\"50
+set encoding=utf-8
+set fileencoding=utf-8
+
+set autoindent
+set smartindent
+
+set clipboard=unnamedplus
+set wrap
+set hlsearch
+set incsearch
+set nowrap
+
+set sessionoptions=blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions
+set list
+set listchars=tab:>-,trail:-,lead:·,eol:¬
+
+set viminfo=
 autocmd BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
 
-" --- Busca ---
-set hlsearch            " destaca resultado da busca
-set incsearch           " busca incremental enquanto digita
-
-" --- Mapeamentos estilo Zed / qualidade de vida ---
 inoremap jj <Esc>       " jj pra sair do modo insert
 
-" Backspace no insert com Ctrl+h
-inoremap <C-h> <BS>
-nnoremap <C-h> X        " Deleta caractere à esquerda no normal mode
-
-" Delete no insert com Ctrl+l
-inoremap <C-l> <Del>
-nnoremap <C-l> x        " Deleta caractere sob o cursor
-
-" --- Navegação entre splits com Ctrl + hjkl ---
-nnoremap <C-j> <C-w>j
-nnoremap <C-k> <C-w>k
-nnoremap <C-h> <C-w>h
-nnoremap <C-l> <C-w>l
-
-" --- Leader Key ---
 let mapleader=" "
 nnoremap <leader>w :w<CR>        " salvar com <space>w
 nnoremap <leader>q :q<CR>        " sair com <space>q
 nnoremap <leader>x :x<CR>        " salvar e sair com <space>x
 nnoremap <leader>h :noh<CR>      " limpar highlight de busca com <space>h
 
-" --- Cursor Shape ---
-" Força o formato do cursor no terminal para resolver conflito com ZSH
+" Cursor Shape
 let &t_SI = "\<Esc>[6 q" " SI = Start Insert mode -> cursor em linha
 let &t_EI = "\<Esc>[2 q" " EI = End Insert mode -> cursor em bloco (Normal mode)
 
-" Cursor shapes por modo (alternativa para gVim/MacVim)
-" normal / visual / command  = bloco
-" insert / cmd-insert / visual-ex = vertical
-" replace / cmd-replace      = horizontal
-" operator-pending           = horizontal maior
 set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
+
