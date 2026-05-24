22dcbcb31b125a454f747966d2faec5b861d1af1
Otávio Miranda <luizomf@gmail.com>
2026-05-24 16:13:19 +0000

    refactor(tmux): improve keybindings and disable problematic pane-name injection
    - Set escape-time to 0 for faster escape key response
    
    - Make C-w binding global by adding -n flag to work outside tmux key mode
    
    - Comment out problematic tmux send-keys pane_name injection that was causing issues

--- tmux/scripts/popup.sh
+++ tmux/scripts/popup.sh
@@ -10,8 +10,9 @@ if [ "$CURRENT_SESSION" = "$SESSION_NAME" ]; then
   # We are inside the popup, so we detach
   tmux detach-client
 else
+  # I dont't remember why I put this line here, but it was causing problems
+  # tmux send-keys "$(tmux display-message -p '#{pane_name}')" C-m
 
-  tmux send-keys "$(tmux display-message -p '#{pane_name}')" C-m
   # We are not in a popup, then check if a session exist
   if ! tmux has-session -t "$SESSION_NAME" 2> /dev/null; then
       # If not, we create it
