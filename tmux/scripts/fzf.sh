e6e17aa6dbd79128ac68eb78db950f7eeae88ad1
Otávio Miranda <luizomf@gmail.com>
2026-08-27 17:44:44 -0300

    refactor(tmux): adjust tmux session manager columns

--- tmux/scripts/fzf.sh
+++ tmux/scripts/fzf.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # The target and path remain searchable, but only the fixed-width row is displayed.
-LIST="$(tmux list-window -a -F $'#S:#I\t#{?window_active,◆, }  #{=14;p14:#{session_name}:#{window_index}}  #{=22;p22:#{?#{@pi_status},#{@pi_status},#{pane_title}}}  #{=10;p10:window_name}  #{window_panes} #{?#{==:#{window_panes},1},pane,panes}\t#{pane_current_path}')"
+# LIST="$(tmux list-window -a -F $'#S:#I\t#{?window_active,◆, }  #{=14;p14:#{session_name}:#{window_index}}  #{=22;p22:#{?#{@pi_status},#{@pi_status},#{pane_title}}}  #{=10;p10:window_name}  #{window_panes} #{?#{==:#{window_panes},1},pane,panes}\t#{pane_current_path}')"
+LIST="$(tmux list-window -a -F $'#S:#I\t#{?window_active,◆, }  #{=24;p24:#{session_name}:#{window_index}}  #{=24;p24:#{?#{@pi_status},#{@pi_status},#{pane_title}}}  #{=24;p24:window_name}')"
 WINDOW_COUNT="$(printf '%s\n' "$LIST" | wc -l | tr -d ' ')"
 POPUP_HEIGHT=$((WINDOW_COUNT + 5))
 ((POPUP_HEIGHT > 20)) && POPUP_HEIGHT=20
