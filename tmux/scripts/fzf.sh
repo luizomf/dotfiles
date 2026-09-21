bd56a19091568de8f99b1e5a14db8fbb3af4e18f
Otávio Miranda <luizomf@gmail.com>
2026-09-21 10:52:15 -0300

    chore(tmux): adjust tmux fzf and popups

--- tmux/scripts/fzf.sh
+++ tmux/scripts/fzf.sh
@@ -22,7 +22,7 @@ printf '%s\n' "$LIST" >"$LIST_FILE"
 FZF_TMUX_CMD=(
   fzf
   --ansi
-  '--color=fg:#eae8ff,fg+:#6bccff,bg:#141418,bg+:#242428,hl:#f0f0ff,hl+:#ffffff,info:#6bccff,marker:#6bccff,prompt:#f0f0ff,spinner:#6bccff,pointer:#6bccff,header:#6bccff,gutter:#1c1c1f,border:#2c2c2f,separator:#2c2c2f,scrollbar:#6bccff,label:#6bccff,query:#ffffff'
+  '--color=fg:#eae8ff,fg+:#6bccff,bg:#141418,bg+:#242428,hl:#f0f0ff,hl+:#ffffff,info:#6bccff,marker:#6bccff,prompt:#f0f0ff,spinner:#6bccff,pointer:#6bccff,header:#6bccff,gutter:#1c1c1f,border:#2c2c2f,separator:#2c2c2f,scrollbar:#6bccff,label:regular:italic:#6bccff,query:regular:italic:#ffffff'
   --border=rounded
   '--border-label= windows '
   --border-label-pos=0
