00ff863e6b2bae51622b9e2ebcaf875a112fb6e5
Otávio Miranda <luizomf@gmail.com>
2026-09-24 15:19:38 -0300

    chore(tmux): resture last tmux conf

--- nvim/lua/settings/theme.lua
+++ nvim/lua/settings/theme.lua
@@ -3,27 +3,27 @@ vim.opt.termguicolors = true
 
 -- DARK THEME
 -- Use omtheme for dark background only
--- vim.cmd("colorscheme omtheme")
+vim.cmd("colorscheme omtheme")
 
--- -- LIGHT THEMES
--- -- For light backgrounds, use the colorscheme you like
-vim.cmd("colorscheme peachpuff")
-
--- -- Remove background to use the terminal background color
-local bg_groups = {
-  "Normal",
-  "NormalNC",
-  "SignColumn",
-  "LineNr",
-  "CursorLineNr",
-  "CursorColumn",
-  "ColorColumn",
-  "CursorLine",
-  "StatusLine",
-  "StatusLineNC",
-  "StatusLineTerm",
-  "StatusLineTermNC",
-}
-for _, group in ipairs(bg_groups) do
-  vim.api.nvim_set_hl(0, group, { bg = "NONE", ctermbg = "NONE" })
-end
+-- -- -- LIGHT THEMES
+-- -- -- For light backgrounds, use the colorscheme you like
+-- vim.cmd("colorscheme peachpuff")
+--
+-- -- -- Remove background to use the terminal background color
+-- local bg_groups = {
+--   "Normal",
+--   "NormalNC",
+--   "SignColumn",
+--   "LineNr",
+--   "CursorLineNr",
+--   "CursorColumn",
+--   "ColorColumn",
+--   "CursorLine",
+--   "StatusLine",
+--   "StatusLineNC",
+--   "StatusLineTerm",
+--   "StatusLineTermNC",
+-- }
+-- for _, group in ipairs(bg_groups) do
+--   vim.api.nvim_set_hl(0, group, { bg = "NONE", ctermbg = "NONE" })
+-- end
